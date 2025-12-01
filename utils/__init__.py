fbaa88d677f45c06bc7e9b053e367ef8436a18a8
FlavRomano <flavio.romano92@gmail.com>
2025-12-01 14:11:55 +0100

    add utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,4 @@
 from .data_loader import get_ml_cup_data
 from .data_loader import get_monk1_data
+
+from .optuna import delete_pruned_trial_dirs
---
+++ utils/optuna.py
@@ -0,0 +1,31 @@
+import shutil
+
+def delete_pruned_trial_dirs(root_path, study):
+    pruned_numbers = {
+        trial.number 
+        for trial in study.trials 
+        if trial.state == optuna.trial.TrialState.PRUNED
+    }
+
+    removed = []
+
+    if not os.path.isdir(root_path):
+        raise ValueError(f"{root_path} is not a valid directory.")
+
+    for entry in os.listdir(root_path):
+        full_path = os.path.join(root_path, entry)
+
+        if entry.startswith("."):
+            continue  # skip hidden folders
+            
+        if entry.count("_OPTUNA_TRIAL#") == 1:
+            try:
+                trial_num = int(entry.split("_OPTUNA_TRIAL#")[1])
+            except ValueError:
+                continue
+
+            if trial_num in pruned_numbers:
+                shutil.rmtree(full_path)
+                removed.append(entry)
+
+    return removed
