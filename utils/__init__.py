21f6cf9fe4a4cebd0d72b741e43e2792d1f0b077
Alexo41 <alexostache41@gmail.com>
2025-12-28 16:10:18 +0100

    prevent crash if Keras/TF is missing: added try-except with warning in utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,15 +4,19 @@ from .data_loader import split_dataloader
 from .data_loader import cv_fold_split
 from .data_loader import apply_pca_on_X
 
-from .keras import load_hyperparameters
-from .keras import load_saved_model
-from .keras import save_hyperparameters
-from .keras import dict_to_filename
-from .keras import build_results_json
-from .keras import make_early_stopping
-from .keras import assessment
-from .keras import save_history
-from .keras import save_history_from_dict
+try:
+    from .keras import load_hyperparameters
+    from .keras import load_saved_model
+    from .keras import save_hyperparameters
+    from .keras import dict_to_filename
+    from .keras import build_results_json
+    from .keras import make_early_stopping
+    from .keras import assessment
+    from .keras import save_history
+    from .keras import save_history_from_dict
+except ImportError as e:
+    print(f"⚠️  Warning: Keras/TensorFlow utils not loaded. Reason: {e}")
+    pass
 
 from .optuna import delete_pruned_trial_dirs
 from .optuna import import_csv
